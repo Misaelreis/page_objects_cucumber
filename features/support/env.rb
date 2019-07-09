2e118f1526916b8a6c13c4dd8049dba59a1edc5c
Misael Aparecido Reis Cândido <misaelcandido@ENG-317.local>
2019-07-09 17:57:08 -0300

    update

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,9 +3,30 @@ require 'selenium-webdriver'
 require 'site_prism'
 require_relative 'page_helper.rb'
 
+BROWSER = ENV['BROWSER']
+AMBIENTE = ENV['AMBIENTE']
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml") #homolog.yml
+
 World(PageObjects)
+Capybara.register_driver :selenium do |app|
+
+    if BROWSER.eql?('chrome')
+        Capybara::Selenium::Driver.new(app, :browser => :chrome)
+        elsif BROWSER.eql?('firefox')
+        Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
+        elsif BROWSER.eql?('ie')
+        Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
+        elsif BROWSER.eql?('safari')
+        Capybara::Selenium::Driver.new(app, :browser => :safari)
+        elsif BROWSER.eql?('poltergeist')
+        options = {js_erros: false}
+        Capybara::Poltergeist::Driver.new(app, options)
+    end
+
+end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = 'https://automacaocombatista.herokuapp.com'
+    config.default_driver = :selenium
+    config.app_host = CONFIG['url_padrao']
     config.default_max_wait_time = 5
 end
