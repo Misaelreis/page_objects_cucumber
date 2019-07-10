7221f7452144b6cad64b49cff372f5b06c79f9ad
Misaelreis <zael.au@gmail.com>
2019-07-09 23:07:07 -0300

    update

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,15 +12,23 @@ Capybara.register_driver :selenium do |app|
 
     if BROWSER.eql?('chrome')
         Capybara::Selenium::Driver.new(app, :browser => :chrome)
-        elsif BROWSER.eql?('firefox')
-        Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
-        elsif BROWSER.eql?('ie')
-        Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
-        elsif BROWSER.eql?('safari')
-        Capybara::Selenium::Driver.new(app, :browser => :safari)
-        elsif BROWSER.eql?('poltergeist')
-        options = {js_erros: false}
-        Capybara::Poltergeist::Driver.new(app, options)
+
+    elsif BROWSER.eql?('chrome_headless')
+        Capybara::Selenium::Driver.new(app, :browser => :chrome,
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+        'chromeOptions' => {'args' =>['--headless', 'disable-gpu']}
+        )
+        )
+
+        #elsif BROWSER.eql?('firefox')
+        #Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
+        #elsif BROWSER.eql?('ie')
+        #Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
+        #elsif BROWSER.eql?('safari')
+        #Capybara::Selenium::Driver.new(app, :browser => :safari)
+        #elsif BROWSER.eql?('poltergeist')
+        #options = {js_erros: false}
+        #Capybara::Poltergeist::Driver.new(app, options)
     end
 
 end
